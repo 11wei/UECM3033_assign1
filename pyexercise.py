7bda709dbd925bf23da0efab63196915014cf213
Soong Ting Wei <soong.tingwei@1utar.my>
2016-01-29 02:00:52 +0800

    Added pyexercise.py

--- pyexercise.py
+++ pyexercise.py
@@ -7,18 +7,27 @@ def fun_1( your_id ):
 
 def my_integral():
     x = sy.Symbol('x')
-    ans = sy.integrate( sy.exp(-x**2), (x,0, sy.oo))
+    ans = sy.integrate( sy.sin(x)/x), ((x,0, sy.oo))
     return ans
 
 def my_solution():
-    A = np.array( [[3,1], [1,2]] )
-    b = np.array([9,8])
+    A = np.array( [[1,4,6,3,6,8,2,4,7,5],
+                   [3,2,4,5,6,7,5,3,1,3],
+                   [2,3,5,6,7,5,3,2,4,5],
+                   [3,6,7,5,3,1,3,4,5,7],
+                   [4,5,6,4,3,2,4,5,6,7],
+                   [6,8,6,3,4,6,7,5,4,5],
+                   [3,4,6,4,3,2,3,5,6,7],
+                   [4,5,3,2,4,5,3,2,4,5],
+                   [5,3,3,5,6,8,6,4,3,2],
+                   [3,4,6,7,6,4,3,2,3,4]])
+    b = np.array([1,2,7,6,5,4,3,2,1,5])
     x = np.linalg.solve(A,b) # Solve Ax = b
     return x
 
 
 if __name__ == '__main__':
-    your_id = 12345678
+    your_id = 1304414
     print('Hexadecimal representation of %d is %s'%( your_id, fun_1( your_id) ))
     print('Integral = ', my_integral())
     print('Solution = ', my_solution())
